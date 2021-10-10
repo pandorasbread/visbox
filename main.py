14c8086935c366334414f51f5ecdbffe696550d9
pandorasbread <pandorasbread@gmail.com>
2021-10-09 23:57:07 -0600

    bars false looks great. bars true truncates quite a bit of data. working on getting 80 bars tho

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def parse_audio_filepath(path):
     return sf.SoundFile(path)
 
 
-def parse_audio(audioData, maxfreq, framerate, blankSecondsAfter):
+def parse_audio(audioData, maxfreq, framerate, blankSecondsAfter, bars):
     audioArray = audioData.read()
     for dat in audioArray:
         monodata.append((dat[0] + dat[1])/2)
@@ -31,7 +31,7 @@ def parse_audio(audioData, maxfreq, framerate, blankSecondsAfter):
     #pyplot.ion()
     #pyplot.show()
 
-    plot_audioFreqData(monodata, int(audioData.samplerate), 1/framerate, 0.025, maxfreq)
+    plot_audioFreqData(monodata, int(audioData.samplerate), 1/framerate, 0.025, maxfreq, bars)
 
 
 
@@ -41,78 +41,60 @@ def plot_audioTimeData(audioData):
     pyplot.title('False Knight, 10-20s, mono')
 
 
-def plot_audioFreqData(audioData, samplerate, skipParamMultiplier, windowMultiplier, maxfreq):
-    tentwentyfour = 1024
-    updateInterval = samplerate / framerate
-    #skip parameter is one over the framerate. we can use it as a multiplier to pass less parameters around.
-    skipParam = int(samplerate * skipParamMultiplier)
-    window = int(samplerate * windowMultiplier)
-    frames = enframe(audioData, skipParam, window)
-    (shortTimeFourierTransform, freqaxis) = stft(frames, len(frames), samplerate)
-
-    pyplot.rcParams.update({
-        "figure.facecolor": (1.0, 0.0, 0.0, 0.0),  # red   with alpha = 0%
-        "axes.facecolor": (0.0, 1.0, 0.0, 0.0),  # green with alpha = 0%
-        "savefig.facecolor": (0.0, 0.0, 1.0, 0.0),  # blue  with alpha = 0%
-    })
+def plot_audioFreqData(audioData, samplerate, skipParamMultiplier, windowMultiplier, maxfreq, bars):
     fig, ax = pyplot.subplots()
-    fig.set_size_inches(10.8, 7.20)
-
-    configurePlot(fig, ax, maxfreq)
-
+    fig.set_size_inches(19.2, 10.8)
+    configurePlot(fig, ax, maxfreq, bars)
     line, = ax.plot([], )
-    fig.canvas.draw()
-    axbackground = fig.canvas.copy_from_bbox(ax.bbox)
-    pyplot.show(block= False)
-    begin_time = datetime.datetime.now()
-    print('0 seconds')
-    #for i in range(len(frames)):
-        #if int(i%updateInterval) == 0:
-        #print(list[i])
-        #if (i % 30 == 0):
-            #print(datetime.datetime.now() - begin_time)
-        #line.set_data(freqaxis[freqaxis <= max_freq], np.log(abs(shortTimeFourierTransform[i][freqaxis <= max_freq])))
-       # line.set_data(freqaxis[freqaxis <= 20000], np.log(np.maximum(1, abs(shortTimeFourierTransform[i][freqaxis <= 20000]) ** 2)))
-        #fig.canvas.restore_region(axbackground)
-       # ax.draw_artist(line)
-       # fig.canvas.blit(ax.bbox)
-       # fig.canvas.flush_events()
-        #drawGraphs(shortTimeFourierTransform, freqaxis, max_freq, i)
-        #pyplot.pause(1 / 30)
-            #framesToDraw.append(audioData[i])
-            #time.sleep(1/updateInterval)
+    updateInterval = samplerate / framerate
+    #skip parameter multiplier is one over the framerate. we can use it as a multiplier to pass less parameters around.
+    skipParam = int(samplerate * skipParamMultiplier)
+    #window is frame length. decreasing it increases smear which could look more pleasing.
+    window = int(samplerate * windowMultiplier)
+    print('Generating spectrogram array...')
+    frames = enframe(audioData, skipParam, window)
+    (shortTimeFourierTransform, freqaxis) = stft(frames, int(len(frames)/80) if bars else len(frames), samplerate)
 
-    anim = animation.FuncAnimation(fig, animate, frames=len(frames)-1, fargs=(freqaxis, shortTimeFourierTransform, line),
+    #spectrumToShow = stft2level(shortTimeFourierTransform, round(len(frames) * maxfreq / samplerate)) if bars else shortTimeFourierTransform
+    spectrumToShow = stft2level(shortTimeFourierTransform,
+                                len(frames)) if bars else shortTimeFourierTransform
+    interval = int(len(spectrumToShow[0]) / 80)
+    newSpecToShow = []
+
+    print('Creating animation...')
+    #create video
+    tenPercentTicks = []
+    tenPercent = round(len(spectrumToShow)/10)
+    for num in range(len(spectrumToShow)):
+        if (num != 0 and num%tenPercent == 0):
+            tenPercentTicks.append(num)
+    anim = animation.FuncAnimation(fig, animate, frames=len(frames)-1, fargs=(freqaxis, spectrumToShow, line, maxfreq, tenPercentTicks, bars),
                                    interval=(skipParamMultiplier)*1000)
+
     fpsCalc = int(1/skipParamMultiplier)
     writervideo = animation.FFMpegWriter(fps=fpsCalc)
     anim.save('./func.mp4', writer=writervideo, dpi=100)
-
+    print('Animation completed. Rendering and saving animation (this can take a while)...')
     input_video = ffmpeg.input('./func.mp4').filter('scale', 1080, -1)
-
     input_audio = ffmpeg.input('./fk.wav')
-
     filename = './song' + datetime.datetime.now().strftime("%H-%M-%S") + '.mp4'
-
+    print('Saving...')
     ffmpeg.concat(input_video, input_audio, v=1, a=1).output(filename, **{'c:v': 'libx265'}, crf=20).run()
-    #ffmpeg.concat(input_video, input_audio, v=1, a=1).output('./finished_video.mp4', **{'qscale:v': 3}).run()
-    #pyplot.close()
-    # 5. Save the animation
-    # anim.save(
-    #    filename='/tmp/sn2011fe_spectral_time_series.mp4',
-    #    fps=24,
-    #    extra_args=['-vcodec', 'libx264'],
-    #    dpi=300,
-    #)
+    print('Waveform complete!')
 
-def animate(frame, axis, stftArray, line):
-    line.set_data(axis[axis <= 20000],
-                  np.log(np.maximum(1, abs(stftArray[frame][axis <= 20000]) ** 2)))
+def animate(frame, axis, stftArray, line, maxfreq, progress, bars):
+    parsedAxis = axis[axis <= maxfreq]
+    line.set_data(axis[axis <= maxfreq],stftArray[frame][axis <= maxfreq]) if bars else line.set_data(axis[axis <= maxfreq], np.log(np.maximum(1, abs(stftArray[frame][axis <= maxfreq]) ** 2)))
+    if frame in progress:
+        progress.pop(0)
+        print('Animation ' + str(100 - (len(progress)*10)) + '% complete')
+    #np.log(np.maximum(1, abs(stftArray[frame][axis <= maxfreq]) ** 2)))
+    #line.set_data(axis, stftArray[frame])
     return line
 
-def configurePlot(fig, ax, maxfreq) :
+def configurePlot(fig, ax, maxfreq, bars) :
     ax.set_xlim(0, maxfreq)
-    ax.set_ylim([-1, 10])
+    ax.set_ylim([-10 if bars else -2, 60 if bars else 10])
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
@@ -121,6 +103,11 @@ def configurePlot(fig, ax, maxfreq) :
     ax.get_yaxis().set_ticks([])
     ax.patch.set_visible(False)
     fig.patch.set_visible(False)
+    pyplot.rcParams.update({
+        "figure.facecolor": (1.0, 0.0, 0.0, 0.0),  # red   with alpha = 0%
+        "axes.facecolor": (0.0, 1.0, 0.0, 0.0),  # green with alpha = 0%
+        "savefig.facecolor": (0.0, 0.0, 1.0, 0.0),  # blue  with alpha = 0%
+    })
 
 def drawGraphs(stft, freqaxis, max_freq, frame):
     #pyplot.clf()
@@ -163,9 +150,6 @@ def getTimeAxisAtFrame(frames, frameNum):
 
 
 def enframe(signal, skipParameter, windowLength):
-    # w = 0.54*np.ones(L)
-    # for n in range(0,L):
-    #   w[n] = w[n] - 0.46*math.cos(2*math.pi*n/(L-1))
     w = np.hamming(windowLength)
     frames = []
     nframes = 1 + int((len(signal) - windowLength) / skipParameter)
@@ -194,12 +178,11 @@ def stft2level(stft_spectra,max_freq_bin):
 if __name__ == '__main__':
     # eventually pull in JSON object with wav, layout, and text data
 
+    bars = False
     maxfreq = 8000
     framerate = 30
     blankSecondsAfter = 2
     audioFile = parse_audio_filepath('fk.wav')
-    parse_audio(audioFile, maxfreq, framerate, blankSecondsAfter)
-
-    input('Press Enter to exit')
+    parse_audio(audioFile, maxfreq, framerate, blankSecondsAfter, bars)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
