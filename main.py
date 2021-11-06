ee5aa62c4be9597acab6e1eb0f66f2975e8e7acd
pandorasbread <pandorasbread@gmail.com>
2021-11-05 22:22:55 -0600

    break out settings into an object

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import datetime
-from datetime import time
 import json
 import pathlib
 from pathlib import Path
-import logging
 import ffmpeg
 import librosa
 from matplotlib import animation
-import matplotlib.animation as anim
 import matplotlib.patheffects as path_effects
 import matplotlib.pyplot as pyplot
 import numpy as np
@@ -15,6 +12,8 @@ import pydub
 
 import drawable
 from drawable import AxisType, ShapeType, Drawable
+from settings import Settings
+
 
 def determineFfmpegPath():
     path = Path('.\\ffmpeg.exe')
@@ -28,13 +27,13 @@ pyplot.rcParams['animation.ffmpeg_path'] = determineFfmpegPath()
 
 
 def loadFromFile():
-    filepath = audio_file_path
+    filepath = settings.audio_file_path
     filetype = pathlib.Path(filepath).suffix
     filename = pathlib.Path(filepath).stem
     print('File type is ' + filetype)
     if (filetype == '.mp3'):
         print('Converting to .wav...')
-        audiodata = pydub.AudioSegment.from_mp3(audio_file_path)
+        audiodata = pydub.AudioSegment.from_mp3(settings.audio_file_path)
         newFilePath = './' + filename + '.wav'
         print('Saving new .wav file...')
         audiodata.export(newFilePath, format='wav')
@@ -55,14 +54,14 @@ def generate_visualizer():
 
 def getSpectrogramAndFrequencyAxisAndMaxDb(timeseries, samplerate):
     # skip parameter multiplier is one over the framerate. we can use it as a multiplier to pass less parameters around.
-    skipParam = int(samplerate * (1 / framerate))
+    skipParam = int(samplerate * (1 / settings.framerate))
 
     shortTimeFourierTransform = np.abs(librosa.stft(timeseries, hop_length=skipParam, n_fft=2048 * 4))
     spectrogram = librosa.amplitude_to_db(shortTimeFourierTransform, ref=np.max)
 
     freqaxis = librosa.core.fft_frequencies(samplerate, 2048 * 4)
 
-    freqaxisTemp = [x for x in freqaxis if x <= max_frequency]
+    freqaxisTemp = [x for x in freqaxis if x <= settings.max_frequency]
 
     freqaxis = freqaxisTemp
     spectrogram = spectrogram[0:len(freqaxis)] + 80
@@ -71,8 +70,8 @@ def getSpectrogramAndFrequencyAxisAndMaxDb(timeseries, samplerate):
 
     newspec = []
     newfreq = []
-    vals = np.arange(0, len(spectrogram) - int(len(spectrogram) / number_of_points),
-                     int(len(spectrogram) / number_of_points))
+    vals = np.arange(0, len(spectrogram) - int(len(spectrogram) / settings.number_of_points),
+                     int(len(spectrogram) / settings.number_of_points))
     for i in vals:
         newspec.append(np.array(spectrogram[i]))
         for j in spectrogram[i]:
@@ -84,31 +83,31 @@ def getSpectrogramAndFrequencyAxisAndMaxDb(timeseries, samplerate):
     spectrogram = np.array(newspec)
 
     spectrogram = spectrogram.T
-    if (polar):
+    if (settings.polar):
         minbarvalue = 10
-        if (not bars):
+        if (not settings.bars):
             minbarvalue = 0
         func = np.vectorize(getPolarValue)
         polarizedspectrogram = func(spectrogram, maxbarvalue, minbarvalue)
         spectrogram = polarizedspectrogram
 
-    if (bars and bar_scale != 1.0):
+    if (settings.bars and settings.bar_scale != 1.0):
         scaledSpec = np.vectorize(applyBarScale)(spectrogram)
         spectrogram = scaledSpec
 
     return spectrogram, freqaxis, maxbarvalue
 
 def arrange_freq_axis_for_chart_type(freqaxisData):
-    if polar:
-        if bars:
+    if settings.polar:
+        if settings.bars:
             indexes = list(range(1, len(freqaxisData) + 1))
             return [element * getBarWidth(len(freqaxisData)) for element in indexes]
         else:
-            return np.linspace(0, 2. * np.pi, num=number_of_points)
+            return np.linspace(0, 2. * np.pi, num=settings.number_of_points)
     return freqaxisData
 
 def applyBarScale(value):
-    return value * bar_scale
+    return value * settings.bar_scale
 
 def makeVideo(fig, freqaxis, dataLength):
     print('Creating animation...')
@@ -116,27 +115,27 @@ def makeVideo(fig, freqaxis, dataLength):
     percentageTicks = getPercentageTicksArray(dataLength)
     anim = animation.FuncAnimation(fig, animate, frames=dataLength - 1, init_func=initAnimation,
                                    fargs=(freqaxis, percentageTicks, drawables), blit = True,
-                                   interval=(1 / framerate) * 1000)
-    writervideo = animation.FFMpegWriter(fps=framerate)
-    animationfilename = pathlib.Path(audio_file_path).stem + ' anim.mp4'
+                                   interval=(1 / settings.framerate) * 1000)
+    writervideo = animation.FFMpegWriter(fps=settings.framerate)
+    animationfilename = pathlib.Path(settings.audio_file_path).stem + ' anim.mp4'
     animStartTime = datetime.datetime.now()
-    anim.save(animationfilename, writer=writervideo, dpi=dpiMultiplier)
+    anim.save(animationfilename, writer=writervideo, dpi=settings.dpiMultiplier)
     delta = datetime.datetime.now() - animStartTime
     print('Animating took ' + str(delta.seconds) + ' seconds.')
     print('Animation completed. Rendering and saving animation (this can take a while)...')
     input_video = ffmpeg.input(animationfilename)
-    input_audio = ffmpeg.input('./' + pathlib.Path(audio_file_path).stem + '.wav')
-    filename = pathlib.Path(audio_file_path).stem + datetime.datetime.now().strftime("%m%d-%H-%M-%S") + '.mp4'
+    input_audio = ffmpeg.input('./' + pathlib.Path(settings.audio_file_path).stem + '.wav')
+    filename = pathlib.Path(settings.audio_file_path).stem + datetime.datetime.now().strftime("%m%d-%H-%M-%S") + '.mp4'
     print('Saving...')
     ffmpeg.concat(input_video, input_audio, v=1, a=1).output(filename, **{'c:v': 'libx265'}, crf=20).run()
     print('Waveform complete!')
     delta2 = datetime.datetime.now() - animStartTime
-    print('Overall, took about ' + str(delta2.seconds) + ' seconds to render a ' + str((dataLength-1)/framerate) + ' second video.')
+    print('Overall, took about ' + str(delta2.seconds) + ' seconds to render a ' + str((dataLength-1)/settings.framerate) + ' second video.')
 
 def getGhostDecay(visualizerData, rate_of_decay):
     decay_data = [np.array([0] * len(visualizerData[0])) for _ in range(len(visualizerData))]
     freq_decay_counter = [1] * len(visualizerData[0])
-    framesBeforeDecrease = round(framerate/6) #1/x of a second seems fine right?
+    framesBeforeDecrease = round(settings.framerate/6) #1/x of a second seems fine right?
     for i in range(len(visualizerData)):
         if i == 0:
             decay_data[i] = visualizerData[i]
@@ -158,22 +157,22 @@ def getGhostDecay(visualizerData, rate_of_decay):
 def createDrawables(fig, spectrumToShow, freqaxis, maxbarvalue):
     visAxisType = AxisType.CARTESIAN
     visShapeType = ShapeType.LINE
-    if (polar):
+    if (settings.polar):
         visAxisType = AxisType.POLAR
-    if (bars):
+    if (settings.bars):
         visShapeType = ShapeType.BAR
 
     #create the ghost drawable
     if (getAxis(fig, -10) != None):
         ghost_color = '#2F2F2F'
-        ghostDecayRate = (-maxbarvalue) / framerate
+        ghostDecayRate = (-maxbarvalue) / settings.framerate
         ghost_decay_data = getGhostDecay(spectrumToShow, ghostDecayRate / 2)
         ghostArtistData = getArtistData(freqaxis, ghost_decay_data[0], getAxis(fig, -10), ghost_color, ghost_color)
         ghost_drawable = Drawable(ghostArtistData, ghost_decay_data, visAxisType, visShapeType, maxbarvalue)
         drawables.append(ghost_drawable)
 
     #create the main plot drawable
-    artistData = getArtistData(freqaxis, spectrumToShow[0], getAxis(fig, 0), bar_color, bar_edge_color)
+    artistData = getArtistData(freqaxis, spectrumToShow[0], getAxis(fig, 0), settings.bar_color, settings.bar_edge_color)
     visualizer_drawable = Drawable(artistData, spectrumToShow, visAxisType, visShapeType, maxbarvalue)
     drawables.append(visualizer_drawable)
 
@@ -191,7 +190,7 @@ def createDrawables(fig, spectrumToShow, freqaxis, maxbarvalue):
 
 def getArtistData(freqaxis, firstFrameData, axis, fillColor, edgeColor):
     artistData = []
-    if (bars):
+    if (settings.bars):
         artistData = getBarData(freqaxis, firstFrameData, axis, fillColor, edgeColor)
     else:
         artistData = getLineData(freqaxis, axis, edgeColor, fillColor)
@@ -214,7 +213,7 @@ def getLineData(freqaxis, ax, linecolor, fillcolor):
 
 def getBarData(freqaxis, firstFrameData, ax, barcolor, baredgecolor):
 
-    if (polar):
+    if (settings.polar):
         return ax.bar(freqaxis, firstFrameData, align='center', width=getBarWidth(len(freqaxis)), bottom=20, color=barcolor,
                       linewidth=1, edgecolor=baredgecolor)
     else:
@@ -225,8 +224,8 @@ def getBarWidth(axisLength = 0):
     # TODO: make this look not-bad at other resolutions
     defaultWidth = 75
     defaultBars = 80
-    barwidth = (defaultWidth) * (defaultBars / number_of_points)
-    if (polar):
+    barwidth = (defaultWidth) * (defaultBars / settings.number_of_points)
+    if (settings.polar):
         barwidth = 2 * np.pi / axisLength
     return barwidth
 
@@ -269,7 +268,7 @@ def draw_line(drawable, freqaxis, frame_num):
     drawable.artist_info[0].set_data(freqaxis, drawable.data[frame_num])
 
     drawable.artist_info[1].axes.collections.clear()
-    fillcolor = bar_color
+    fillcolor = settings.bar_color
     if (drawable.artist_info[1].axes.zorder == -10):
         fillcolor = '#2F2F2F'
     drawable.artist_info[1].axes.fill_between(freqaxis, 0, drawable.data[frame_num], facecolor = fillcolor)
@@ -279,22 +278,22 @@ def getPolarValue(value, maxValue, minValue = 10):
     return slope * (value) + minValue
 
 def configurePlot():
-    if (polar):
+    if (settings.polar):
         fig, visualizer_axis = pyplot.subplots(subplot_kw=dict(projection="polar"))
         cleanPolarPlot(visualizer_axis)
     else:
         fig, visualizer_axis = pyplot.subplots()
         cleanCartesianPlot(visualizer_axis)
 
-    if (ghost):
+    if (settings.ghost):
         addGhostVisualizer(fig)
-    fig.set_size_inches(resolution_x/dpiMultiplier, resolution_y/dpiMultiplier)
+    fig.set_size_inches(settings.resolution_x/settings.dpiMultiplier, settings.resolution_y/settings.dpiMultiplier)
 
     setAxis(visualizer_axis)
 
 
     createBackground(fig, visualizer_axis)
-    if (text != ''):
+    if (settings.text != ''):
         addText(fig)
 
     return fig
@@ -302,8 +301,8 @@ def configurePlot():
 def setAxis(axis):
 
     axis.set_ylim([0, 80])
-    if (not bars) and (not polar):
-        axis.set_xlim([0, max_frequency])
+    if (not settings.bars) and (not settings.polar):
+        axis.set_xlim([0, settings.max_frequency])
 
 
 def cleanPolarPlot(axis):
@@ -322,17 +321,17 @@ def cleanCartesianPlot(axis):
 
 
 def createBackground(fig, ax):
-    if (bk_img_path != ''):
+    if (settings.bk_img_path != ''):
         print('!!! IMAGE SPECIFIED, THIS WILL TAKE A LOT LONGER TO RENDER, SORRY!!!')
-        img = pyplot.imread(bk_img_path)
+        img = pyplot.imread(settings.bk_img_path)
         ax_image = createBackgroundAxisIfNotExists(fig)
         cleanCartesianPlot(ax_image)
-        ax_image.imshow(img, extent=(0,resolution_x,0,resolution_y), resample=False)
+        ax_image.imshow(img, extent=(0,settings.resolution_x,0,settings.resolution_y), resample=False)
         fig.patch.set_visible(False)
-    elif (bk_img_color != ''):
+    elif (settings.bk_img_color != ''):
         ax_image = createBackgroundAxisIfNotExists(fig)
-        ax_image.set_facecolor(bk_img_color)
-        fig.set_facecolor(bk_img_color)
+        ax_image.set_facecolor(settings.bk_img_color)
+        fig.set_facecolor(settings.bk_img_color)
     else:
         fig.patch.set_visible(False)
         pyplot.rcParams.update({
@@ -347,15 +346,15 @@ def createBackground(fig, ax):
 
 def addText(fig):
     ax_image = createBackgroundAxisIfNotExists(fig)
-    message = ax_image.text(0.5, 0.05, text, fontsize='50', horizontalalignment='center', verticalalignment='center',
-                            color=text_color,
+    message = ax_image.text(0.5, 0.05, settings.text, fontsize='50', horizontalalignment='center', verticalalignment='center',
+                            color=settings.text_color,
                             transform=ax_image.transAxes)
-    if (use_text_outline):
-        message.set_path_effects([path_effects.Stroke(linewidth=text_outline_width, foreground=text_outline_color),
+    if (settings.use_text_outline):
+        message.set_path_effects([path_effects.Stroke(linewidth=settings.text_outline_width, foreground=settings.text_outline_color),
                                   path_effects.Normal()])
 
 def addGhostVisualizer(fig):
-    if (polar):
+    if (settings.polar):
         ghost_axis = fig.add_subplot(polar=True, zorder=-10)
         cleanPolarPlot(ghost_axis)
     else:
@@ -373,59 +372,16 @@ def createBackgroundAxisIfNotExists(fig):
 def getAxis(fig, zorder):
     return next((axis for axis in fig.axes if axis.zorder == zorder), None)
 
-def getxyResolutions(resolutionOption):
-    if (resolutionOption == "4k"):
-        return 3840, 2160
-    elif (resolutionOption == "1080p"):
-        return 1920, 1080
-    elif (resolutionOption == "twitter"):
-        return 1000, 1000
-    elif (resolutionOption == "720p"):
-        return 1280, 720
-    else:
-        return 1920, 1080
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # eventually pull in JSON object with wav, layout, and text data
     with open('./config.json') as f:
-        data = json.load(f)
+        json_settings = json.load(f)
 
-    number_of_points = data["number_of_bars"]
-    polar = data["polar"]
-    bars = True
-    # I'm pretty sure i can get a neat polar line. Might redo this later.
-    if (number_of_points == 0):
-        bars = False
-        number_of_points = 80
-    #We need to do hacky shit to have a default amount of 80 line points.. ehhghhhhh
-
-
-    # 10k is pretty good for straight bars, 8k looks good in a circle
-    max_frequency = data["max_frequency"]
-    bar_color = data["bar_color"]
-    bar_edge_color = data["bar_edge_color"]
-    if (bar_edge_color == ""):
-        bar_edge_color = bar_color
-    bar_scale = data["bar_scale"]
-    framerate = data["framerate"]
-    bk_img_path = data["background_img_path"]
-    bk_img_color = data["background_color"]
-    audio_file_path = data["audio_file_path"]
-    resolution_type = data["resolution"]
-    resolution_x, resolution_y = getxyResolutions(resolution_type)
-
-    text = data["text"]
-    text_color = data["text_color"]
-    if (text_color == ''):
-        text_color = '#FFFFFF'
-    use_text_outline = data["use_text_outline"]
-    text_outline_color = data["text_outline_color"]
-    if (text_outline_color == ''):
-        text_outline_color = text_color
-    text_outline_width = data["text_outline_width"]
-    ghost = data["ghost"]
+    settings = Settings(json_settings)
     drawables = []
-    dpiMultiplier = 100
+
     generate_visualizer()
 
 
---
+++ settings.py
@@ -0,0 +1,50 @@
+class Settings:
+
+    def __init__(self, data):
+        self.number_of_points = data["number_of_bars"]
+        self.polar = data["polar"]
+        self.bars = True
+        if (self.number_of_points == 0):
+            self.bars = False
+            self.number_of_points = 80
+        # We need to do hacky shit to have a default amount of 80 line points.. ehhghhhhh
+
+        # 10k is pretty good for straight bars, 8k looks good in a circle
+        self.max_frequency = data["max_frequency"]
+        self.bar_color = data["bar_color"]
+        if self.bar_color == '':
+            self.bar_color = '#000000'
+        self.bar_edge_color = data["bar_edge_color"]
+        if (self.bar_edge_color == ""):
+            self.bar_edge_color = self.bar_color
+        self.bar_scale = data["bar_scale"]
+        self.framerate = data["framerate"]
+        self.bk_img_path = data["background_img_path"]
+        self.bk_img_color = data["background_color"]
+        self.audio_file_path = data["audio_file_path"]
+        self.resolution_type = data["resolution"]
+        self.resolution_x, self.resolution_y = self.getxyResolutions(self.resolution_type)
+
+        self.text = data["text"]
+        self.text_color = data["text_color"]
+        if (self.text_color == ''):
+            self.text_color = '#FFFFFF'
+        self.use_text_outline = data["use_text_outline"]
+        self.text_outline_color = data["text_outline_color"]
+        if (self.text_outline_color == ''):
+            self.text_outline_color = self.text_color
+        self.text_outline_width = data["text_outline_width"]
+        self.ghost = data["ghost"]
+        self.dpiMultiplier = 100
+
+    def getxyResolutions(self, resolutionOption):
+        if (resolutionOption == "4k"):
+            return 3840, 2160
+        elif (resolutionOption == "1080p"):
+            return 1920, 1080
+        elif (resolutionOption == "twitter"):
+            return 1000, 1000
+        elif (resolutionOption == "720p"):
+            return 1280, 720
+        else:
+            return 1000, 1000
